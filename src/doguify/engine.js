6ad2c84c6dc2b6ed5ec18f3c896175e8d73e2489
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-19 19:29:42 -0300

    melhoria na engine do doguify

--- src/doguify/engine.js
+++ src/doguify/engine.js
@@ -1,42 +1,37 @@
-export const doguify = (phrase) => {
-    return phrase
-      .split(' ')
-      .map((word) => {
-        const wordLetters = word.split('')
-        return wordLetters
-          .map((letter, index) => {
-            const charCode = letter.charCodeAt(0);
-            const binary = charCode
-              .toString(2)
-              .padStart(8, 0)
- 
-            const auaus = binary
-              .replace(/0/g, 'au, ')
-              .replace(/1/g, 'au au, ')
-              .replace(/, $/, '')
- 
-            return auaus
-          })
-          .join('rrrrrr')
+export const doguify = (string) => {
+  return string
+    .split(' ').map((word) => {
+      const letters = word.split('')
+      return letters.map((letter, _index) => {
+        const charCode = letter.charCodeAt(0);
+        const binary = charCode.toString(2).padStart(8, 0)
+
+        const auaus = binary
+          .replace(/0/g, 'au, ')
+          .replace(/1/g, 'au au, ')
+          .replace(/, $/, '')
+
+        return auaus
       })
-      .join('~')
-  }
- 
-  export const desdoguify = (latido) => {
-    return latido
-      .split('~')
-      .map((word) => {
-        return word
-            .split('rrrrrr')
-         .map((letter) => {
-            const binary = letter
-              .replace(/au au(, )*/g, '1')
-              .replace(/au(, )*/g, '0')
- 
-            return String.fromCharCode(parseInt(binary, 2))
-          })
-          .join('')
-      })
-      .join(' ')
-  }
-  
+        .join('rrrrrr')
+    })
+    .join('ão! ')
+}
+
+export const desdoguify = (latido) => {
+  return latido
+    .split('ão! ')
+    .map((word) => {
+      return word
+        .split('rrrrrr')
+        .map((letter) => {
+          const binary = letter
+            .replace(/au au(, )*/g, '1')
+            .replace(/au(, )*/g, '0')
+
+          return String.fromCharCode(parseInt(binary, 2))
+        })
+        .join('')
+    })
+    .join(' ')
+}
