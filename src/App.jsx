bb97c7c458e869b9804fa0f88f068696f4269036
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-19 01:01:48 -0300

    lógica de codificação e exibição inicialmente concluída

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
 import './App.css'
+import { doguinhofy } from './doguify/engine';
 
 function App () {
+
+
+  const action = () => {
+    const resultText = document.querySelector('.resultText');
+
+    if (resultText) {
+      resultText.remove()
+    }
+
+    const text = document.querySelector('textarea').value;
+    const resultado = document.querySelector(".resultado");
+    const textContainer = document.createElement('p');
+    textContainer.className = "resultText";
+    const doguificar = doguinhofy(text);
+    resultado.appendChild(textContainer).innerHTML = doguificar;
+  }
 
   return (
     <div className="App">
       <header>
         <h1>Doguify</h1>
       </header>
-      <main>
+      <main className='container'>
         <form action="texto">
           <label htmlFor="texto"></label>
-          <textarea name="texto" id="doguificar"></textarea>
+          <textarea name="texto" placeholder='Digite um texto'></textarea>
           <div>
-            <button type="submit">
+            <button type="button" onClick={action}>
               <span>
                 Doguify!
               </span>
             </button>
           </div>
         </form>
-        <div className=''>
-
+        <div className='resultado'>
+          <h3>
+            O resultado será exibido aqui:
+          </h3>
         </div>
       </main>
     </div>
---
+++ src/doguify/engine.js
@@ -0,0 +1,42 @@
+export const doguinhofy = (phrase) => {
+    return phrase
+      .split(' ')
+      .map((word) => {
+        const wordLetters = word.split('')
+        return wordLetters
+          .map((letter, _index) => {
+            const charCode = letter.charCodeAt(0);
+            const binary = charCode
+              .toString(2)
+              .padStart(8, 0)
+ 
+            const auaus = binary
+              .replace(/0/g, 'au, ')
+              .replace(/1/g, 'au au, ')
+              .replace(/, $/, '')
+ 
+            return auaus
+          })
+          .join('rrrrrr')
+      })
+      .join('~')
+  }
+ 
+  export const desdoguinhofy = (latido) => {
+    return latido
+      .split('~')
+      .map((word) => {
+        return word
+            .split('-')
+         .map((letter) => {
+            const binary = letter
+              .replace(/au au(, )*/g, '1')
+              .replace(/au(, )*/g, '0')
+ 
+            return String.fromCharCode(parseInt(binary, 2))
+          })
+          .join('')
+      })
+      .join(' ')
+  }
+  
