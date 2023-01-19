1ea6bf4779d980a5d08479d70c0587212e99bb35
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-19 17:29:52 -0300

    Camada de estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import './App.css'
+import dog from '../public/cachorro.png'
 import { desdoguinhofy, doguinhofy } from './doguify/engine';
 
 function App () {
 
-  const [engine, setEngine] = useState ('');
+  const [engine, setEngine] = useState('');
 
   const clipboard = () => {
     const resultText = document.querySelector('.resultText').textContent;
@@ -13,7 +14,10 @@ function App () {
 
   const handleEngine = ({ target }) => {
     const method = document.querySelector(".method")
+    const trad = document.querySelector(".traducao");
     method.innerHTML = `Traduzindo do ${target.textContent}`;
+
+    target.name === 'doguificar' ? trad.innerHTML = 'Como o doguinho diria:' : trad.innerHTML = 'O que o doguinho quis dizer:';
     setEngine(target.name);
   }
 
@@ -24,10 +28,10 @@ function App () {
       resultText.remove()
     }
 
-    
+
 
     const resultado = document.querySelector(".resultado");
-    const textContainer = document.createElement('p');
+    const textContainer = document.createElement('textarea');
     textContainer.className = "resultText";
     resultado.appendChild(textContainer).innerHTML = engine;
   }
@@ -43,41 +47,52 @@ function App () {
 
   return (
     <div className="App">
-      <header>
-        <h1>Doguify</h1>
-          <div className='caramelo-pic'></div>
-      </header>
+    
       <main className='container'>
+      <header>
+        <div className='header-container'>
+          <div className='header-wrapper'>
+        <h1>Doguify</h1>
+        <img src={ dog } alt="" />
+        </div>
+        </div>
+      </header>
         <form action="texto" onSubmit={(e) => handlerOption(e)}>
-        <p className='method'>Selecione um método de tradução</p>
           <label htmlFor="texto"></label>
-          <textarea name="texto" placeholder='Digite um texto' required></textarea>
+          <p className='description'>
+            Traduza do humanês 👩🏾‍🦱para o cachorrês 🐶 e vice-versa e converse com o seu doguinho.
+          </p>
+          <textarea name="texto" placeholder='Digite alguma coisa para que a nossa equipe canina possa verificar' required></textarea>
           <div className='btn-div'>
+            <p className='method'>Selecione um método de tradução</p>
             <div className='btn-group'>
               <button name='doguificar' type='button' onClick={handleEngine}>
                 Humanês ➡️ Doguês
               </button>
               <button name='desdoguificar' type='button' onClick={handleEngine}>
-                Doguês ➡️ Humanês 
+                Doguês ➡️ Humanês
               </button>
             </div>
-            <button type="submit">
+            <button type="submit" className='doguify'>
               <span>
-                Doguify!
+                Doguify! 🐕
               </span>
             </button>
           </div>
         </form>
         <div className='resultado'>
-          <h3>
-            O resultado será exibido aqui:
+          <h3 className='traducao'>
+            Tradução:
           </h3>
-     
+
         </div>
         <button className='copy-btn' onClick={clipboard}>
-          Copiar tradução
+          Copiar tradução <i class="bi bi-clipboard-check"></i>
         </button>
       </main>
+      <footer>
+        Jr Gouveia - 2023 
+      </footer>
     </div>
 
   )
