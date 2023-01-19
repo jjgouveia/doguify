508c9c1275bf001e38a74b910e41f4483838c61b
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-19 20:16:11 -0300

    Corrigindo link do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/Footer.css'
 export default function Footer () {
     return (
         <footer>
-            <i class="bi bi-github"></i> <a href='https://github.com.com/jjgouveia' target="_blank">Jr Gouveia</a> - 2023 | O conteúdo do Doguify é, puramente, cômico. Você não precisa dele para falar com o seu doguinho.
+            <i class="bi bi-github"></i> <a href='https://github.com/jjgouveia' target="_blank">Jr Gouveia</a> - 2023 | O conteúdo do Doguify é, puramente, cômico. Você não precisa dele para falar com o seu doguinho.
         </footer>
     )
 }
